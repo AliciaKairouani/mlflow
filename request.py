bb8bb828c7126e16dc42e709e41baf49ef495b6e
GULDENER Camille <98022298+GuldenerCamille@users.noreply.github.com>
2023-12-20 11:50:02 +0100

    add a fetch question

--- request.py
+++ request.py
@@ -13,3 +13,19 @@ questions = site.fetch('questions',
 )
 
 print(questions)
+
+
+# Fetch questions from Stack Overflow
+questions = SITE.fetch('questions', tagged='python', sort='votes', order='desc')
+
+filtered_questions = []
+
+# Filter questions
+for question in questions['items']:
+    view_count = question.get('view_count', 0)
+    score = question.get('score', 0)
+    answer_count = question.get('answer_count', 0)
+    tags = question.get('tags', [])
+
+    if view_count > 10 and score > 5 and answer_count > 0 and len([tag for tag in tags if '<' in tag]) >= 5:
+        filtered_questions.append(question)
